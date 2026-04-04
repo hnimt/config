59b9b0b30eabd952433e9c323c0eff9749daec43
minhtc <minh@MacBook-Pro.local>
2026-04-04 23:27:12 +0700

    update

--- nvim/README.md
+++ nvim/README.md
@@ -2,3 +2,8 @@
 
 A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
 Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+
+If had error with mason run it:
+rm -rf ~/.local/share/nvim ~/.local/state/nvim ~/.cache/nvim
+rm -rf ~/.cache/jdtls
+
--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -2,6 +2,7 @@ return {
     {
         "mason-org/mason.nvim",
         opts = {
+            log_level = vim.log.levels.DEBUG,
             pip = {
                 -- This forces Mason to use uv for all python-based package installs
                 install_args = { "--index-url", "https://pypi.org/simple" }, 
@@ -9,3 +10,4 @@ return {
         },
     }
 }
+
