a7fcc63eb537e4aa1e78396e4b95c328c34ba5e3
minhtc <minh@MacBook-Pro.local>
2026-04-05 22:16:01 +0700

    update

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -17,12 +17,12 @@ vim.keymap.set("n", "<C-CR>", "o<Esc>", { desc = "New line above" })
 vim.keymap.set("n", "<S-CR>", "O<Esc>", { desc = "New line above" })
 
 vim.keymap.set("n", "<leader>t", "<leader>ft", { remap = true })
-vim.keymap.set("n", "<C-F>", "*", { remap = true })
+vim.keymap.set("n", "<S-F>", "*", { remap = true })
 
--- Move text up and down
-vim.keymap.set("n", "<C-j>", ":m .+1<CR>==", { desc = "Move line down" })
-vim.keymap.set("n", "<C-k>", ":m .-2<CR>==", { desc = "Move line up" })
-vim.keymap.set("i", "<C-j>", "<Esc>:m .+1<CR>==gi", { desc = "Move line down (Insert)" })
-vim.keymap.set("i", "<C-k>", "<Esc>:m .-2<CR>==gi", { desc = "Move line up (Insert)" })
-vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
-vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+-- Normal Mode
+vim.keymap.set("n", "∆", ":m .+1<CR>==", { silent = true })
+vim.keymap.set("n", "˚", ":m .-2<CR>==", { silent = true })
+
+-- Visual Mode
+vim.keymap.set("v", "∆", ":m '>+1<CR>gv=gv", { silent = true })
+vim.keymap.set("v", "˚", ":m '<-2<CR>gv=gv", { silent = true })
