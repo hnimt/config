f4b0b7168d542583c54b4054f008259279614881
minhtc <minh@MacBook-Pro.local>
2026-04-05 11:44:19 +0700

    update

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,13 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-
 -- Normal mode: Toggle comment for the current line
-vim.keymap.set('n', '<C-/>', 'gcc', { remap = true })
-vim.keymap.set('n', '<C-_>', 'gcc', { remap = true })
+vim.keymap.set("n", "<C-/>", "gcc", { remap = true })
+vim.keymap.set("n", "<C-_>", "gcc", { remap = true })
 -- Visual mode: Toggle comment for the selection
-vim.keymap.set('v', '<C-/>', 'gc', { remap = true })
-vim.keymap.set('v', '<C-_>', 'gc', { remap = true })
+vim.keymap.set("v", "<C-/>", "gc", { remap = true })
+vim.keymap.set("v", "<C-_>", "gc", { remap = true })
 
 -- Close current file (buffer) with leader + w
 vim.keymap.set("n", "<leader>w", "<leader>bd", { remap = true, desc = "Close Current File" })
@@ -17,4 +16,5 @@ vim.keymap.set("n", "<leader>w", "<leader>bd", { remap = true, desc = "Close Cur
 vim.keymap.set("n", "<C-CR>", "o<Esc>", { desc = "New line above" })
 vim.keymap.set("n", "<S-CR>", "O<Esc>", { desc = "New line above" })
 
-vim.keymap.set("n", "<leader>t", "<leader>ft", {remap = true})
+vim.keymap.set("n", "<leader>t", "<leader>ft", { remap = true })
+vim.keymap.set("n", "<S-F>", "*", { remap = true })
