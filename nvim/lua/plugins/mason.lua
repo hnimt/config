d53b79259e13b68f15ad3c768ae94710dfe0c74c
minhtc21 <minhtc21@fpt.com>
2026-04-06 16:05:57 +0700

    update

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,13 +1,12 @@
 return {
-    {
-        "mason-org/mason.nvim",
-        opts = {
-            log_level = vim.log.levels.DEBUG,
-            pip = {
-                -- This forces Mason to use uv for all python-based package installs
-                install_args = { "--index-url", "https://pypi.org/simple" }, 
-            },
-        },
-    }
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      log_level = vim.log.levels.DEBUG,
+      -- pip = {
+      --     -- This forces Mason to use uv for all python-based package installs
+      --     install_args = { "--index-url", "https://pypi.org/simple" },
+      -- },
+    },
+  },
 }
-
--- nvim/lua/plugins/python.lua
+++ nvim/lua/plugins/python.lua
@@ -2,30 +2,12 @@ return {
   {
     "benomahony/uv.nvim",
     -- This ensures it loads when you open a Python file
-    ft = "python", 
+    ft = "python",
     opts = {
       auto_activate_venv = true,
     },
-    -- In LazyVim, we often want to make sure it runs 
+    -- In LazyVim, we often want to make sure it runs
     -- before the LSP attaches
-    lazy = false, 
-  },
-  {
-    "mfussenegger/nvim-dap-python",
-    keys = {
-      {
-        "<leader>dPt",
-        function()
-          require("dap-python").test_method()
-        end,
-        desc = "Debug Method",
-        ft = "python",
-      },
-    },
-    config = function()
-      -- Automatically use the .venv created by 'uv sync' or 'uv venv'
-      local path = vim.fn.getcwd() .. "/.venv/bin/python"
-      require("dap-python").setup(path)
-    end,
+    lazy = false,
   },
 }
