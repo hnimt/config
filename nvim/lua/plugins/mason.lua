638beef34b0c50a4d3722ad8c188387d3def68b3
minhtc21 <minhtc21@fpt.com>
2026-04-07 13:35:13 +0700

    update

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -3,10 +3,6 @@ return {
     "mason-org/mason.nvim",
     opts = {
       log_level = vim.log.levels.DEBUG,
-      -- pip = {
-      --     -- This forces Mason to use uv for all python-based package installs
-      --     install_args = { "--index-url", "https://pypi.org/simple" },
-      -- },
     },
   },
 }
