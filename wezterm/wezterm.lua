89d2df71a732e8131953a9c97ae81237058f3a64
minhtc21 <minhtc21@fpt.com>
2026-04-22 13:16:52 +0700

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,29 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+local act = wezterm.action
+
 config.font_size = 13.0
+
+config.keys = {
+	-- Split panes
+	{ key = "v", mods = "CTRL|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "h", mods = "CTRL|SHIFT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+
+	-- Navigate panes (vim style)
+	{ key = "h", mods = "ALT", action = act.ActivatePaneDirection("Left") },
+	{ key = "l", mods = "ALT", action = act.ActivatePaneDirection("Right") },
+	{ key = "k", mods = "ALT", action = act.ActivatePaneDirection("Up") },
+	{ key = "j", mods = "ALT", action = act.ActivatePaneDirection("Down") },
+
+	-- Resize panes
+	{ key = "LeftArrow", mods = "CTRL|ALT", action = act.AdjustPaneSize({ "Left", 5 }) },
+	{ key = "RightArrow", mods = "CTRL|ALT", action = act.AdjustPaneSize({ "Right", 5 }) },
+	{ key = "UpArrow", mods = "CTRL|ALT", action = act.AdjustPaneSize({ "Up", 5 }) },
+	{ key = "DownArrow", mods = "CTRL|ALT", action = act.AdjustPaneSize({ "Down", 5 }) },
+
+	-- Close pane
+	{ key = "w", mods = "CTRL|SHIFT", action = act.CloseCurrentPane({ confirm = true }) },
+}
 
 return config
